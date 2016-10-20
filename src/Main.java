be17483a28a591d801499ad1240581687d4093cf
nyashacsmith <nyashacsmith@gmail.com>
2016-10-20 18:54:06 -0400

    Everything

--- src/Main.java
+++ src/Main.java
@@ -5,45 +5,88 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Locale;
 
 public class Main extends JFrame {
     private Startpanel startpanel;
 
+    JFrame frame = new JFrame ();
+
+    JPanel j = new JPanel(new GridBagLayout());
+    Startpanel p = new Startpanel();
+
+    public Main(){
+        startpanel = new Startpanel();
+        setTitle("Collage!");
+        setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        setSize(400, 600);
+//frame.getContentPane().setLayout(new GridLayout(40, 60));
+        add(new Startpanel());
+        setVisible(true);
+    }
+
+
     public Startpanel getStartpanelPanel() {
         return startpanel;
     }
-    public class Startpanel {
-        public Startpanel(){
 
+    public class Startpanel extends JPanel {
+        boolean homeScreen = false;
+        boolean toCategories = false;
+        boolean toQuestions = false;
+
+
+        public Startpanel(){//Figuring out how to color background without wrting over buttons
+            homeScreen = true;
+            add (new JButton ("Start!"));//Implement action listens
+            add (new JButton ("Settings!"));
+            add (new JButton ("Leaderboard!"));
         }
 
+//        @Override
+//        public void paint (Graphics g){
+//            super.paint(g);
+//            g.fillRect(0, 0, 800, 900);
+//            // create background
+//            g.setColor(Color.BLACK);
+//
+//        }
+
+
+
+
+
+
+
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Hi Cierra!
-
-
 
 
 
     public static void main(String[] args){
 
         Main m = new Main();
-        m.setVisible(true);
-        m.setSize(600,400);
-        m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        m.setVisible(true);
+//        m.setSize(600,400);
+//        m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }

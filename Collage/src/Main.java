a276a0d3a5a0ba076a256131f9a66378fcf39b4c
nyashacsmith <nyashacsmith@gmail.com>
2016-10-20 16:37:47 -0400

    Add files via upload

---
+++ Collage/src/Main.java
@@ -0,0 +1,36 @@
+
+import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args){
+
+        Main m = new Main();
+    }
+}

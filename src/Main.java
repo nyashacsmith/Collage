7f090511f426b526441db21c616a28a2767d6ecd
Matthew James <aereostophanes@matthews-mbp-4.spelman.edu>
2016-10-24 20:19:18 -0400

    Added questions flow

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,6 @@ import java.awt.event.ActionListener;
 import java.util.Locale;
 
 public class Main extends JFrame {
-//    @Override
-//    public void paint (Graphics g){
-//        super.paint(g);
-//        g.fillRect(0, 0, 800, 900);
-//        // create background
-//        g.setColor(Color.BLACK);
-//
-//    }
-    //private Startpanel startpanel;
-
-
     JFrame frame;
     JPanel home;
 
@@ -32,8 +21,6 @@ public class Main extends JFrame {
     JButton music;
     JButton veganism;
     JButton random;
-    //JPanel j = new JPanel(new GridBagLayout());
-    //Startpanel home;
 
     public Main(){
         frame = new JFrame();
@@ -48,7 +35,7 @@ public class Main extends JFrame {
                 disney = new JButton("Disney!");
                 anime = new JButton("Anime/Manga!");
                 animals = new JButton("Animals!");
-                veganism = new JButton("Veganism :(");
+                veganism = new JButton("Veganism!");
                 music = new JButton("Music!");
                 random = new JButton("Random!");
                 categories.add(disney);
@@ -64,10 +51,17 @@ public class Main extends JFrame {
             }
         });
 
+        /*disney.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });*/
+
         frame.setTitle("Collage!");
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setSize(400, 600);
-        
+
         home.add(start);
         home.add(settings);
         home.add(leaderboard);
@@ -76,42 +70,7 @@ public class Main extends JFrame {
         frame.setVisible(true);
     }
 
-
-    /*public Startpanel getStartpanelPanel() {
-        return startpanel;
-    }*/
-
-    /*public class Startpanel extends JPanel {
-        boolean homeScreen = false;
-        boolean toCategories = false;
-        boolean toQuestions = false;
-
-
-        public Startpanel() {//Figuring out how to color background without writing over buttons
-            homeScreen = true;
-            JButton start = new JButton("Start!");
-            start.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    JPanel category = new JPanel();
-                    frame.getContentPane().add(category);
-                }
-            })
-            
-            JButton settings = new JButton("Settings!");
-            JButton leaderboard = new JButton("Leaderboard!");
-            add(start);
-            add(settings);
-            add(leaderboard);
-        }
-    }*/
-
-
-
     public static void main(String[] args){
         Main m = new Main();
-        //m.setVisible(true);
-        //m.setSize(600,400);
-        //m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
